9e8db1c1c4c27207e029da5f27146ca4038b3cd2
Führar <214996982+vuhrar@users.noreply.github.com>
2026-07-04 02:08:39 +0300

    Initialize SQLAlchemy engine with settings

---
+++ backend/database/engine.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+from backend.core.settings import settings
+
+
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    future=True,
+)
